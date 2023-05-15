864d1b3f0d56f10f07d0ca24541e7695ffda42cb
Evgeny <evgshul@gmail.com>
2023-05-15 17:28:36 +0300

    Removed unused import

--- src/main/java/org/acme/RegistrationDTO.java
+++ src/main/java/org/acme/RegistrationDTO.java
@@ -3,7 +3,6 @@ package org.acme;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 /**
